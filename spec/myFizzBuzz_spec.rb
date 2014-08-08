0d2577121a81e99a1e557648d0ac128cfaa85175
benjamintillett <benjamin.tillett@gmail.com>
2014-08-08 22:18:54 +0100

    add is_divisible_by_three? function

---
+++ spec/myFizzBuzz_spec.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+require 'myFizzBuzz'
+
+describe "fizzbuzz" do 
+	it 'knows 3 is divisible by 3' do 
+		expect(is_divisible_by_three?(3)).to be true
+	end
+	it 'knows 3 is not divisible by 1' do 
+		expect(is_divisible_by_three?(1)).to be false
+	end
+end
