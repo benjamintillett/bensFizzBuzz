dfba9385d2e79b591da868affbf3cb87cf82cb08
benjamintillett <benjamin.tillett@gmail.com>
2014-08-09 07:08:24 +0100

    add is_divisible_by_five? function

--- lib/myFizzBuzz.rb
+++ lib/myFizzBuzz.rb
@@ -1,3 +1,6 @@
 def is_divisible_by_three?(number)
 	number % 3 == 0 
 end
+def is_divisible_by_five?(number)
+	number % 5 == 0 
+end
--- spec/myFizzBuzz_spec.rb
+++ spec/myFizzBuzz_spec.rb
@@ -8,4 +8,10 @@ describe "fizzbuzz" do
 	it 'knows 3 is not divisible by 1' do 
 		expect(is_divisible_by_three?(1)).to be false
 	end
+	it 'knows 5 is divisible by 5' do 
+		expect(is_divisible_by_five?(5)).to be true
+	end
+	it 'knows 5 is not divisible by 1' do 
+		expect(is_divisible_by_five?(1)).to be false
+	end	
 end
